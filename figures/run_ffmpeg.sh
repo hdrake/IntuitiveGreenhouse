6a072721732c3a05380655e0e80ca5a92311de3d
Henri Drake <henrifdrake@gmail.com>
2019-06-04 22:51:02 -0400

    Updated

--- figures/run_ffmpeg.sh
+++ figures/run_ffmpeg.sh
@@ -1,1 +1,1 @@
-ffmpeg -framerate 10 -i %04d.png -vframes 499 -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" IntuitiveGreenhouseEffect.mp4
+ffmpeg -framerate 30 -i %04d.png -vframes 499 -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" IntuitiveGreenhouseEffect.mp4
